33798b3fc0e7369d0bb8964eecb466a8051bd3af
ABIJAM <ABIJAM@LAPTOP-ES1P13CD>
2021-08-18 13:39:38 +0530

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class Item
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int Id { get; set; }
+
+        public string Borrower { get; set; }
+        public string Lender { get; set; }
+
+        public string ItemName { get; set; }
+
+    }
+}
